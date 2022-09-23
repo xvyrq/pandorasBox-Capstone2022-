e076b7392bef7a9b815e73617855fe87da39381a
Luke Manary <69057730+xvyrq@users.noreply.github.com>
2022-09-23 14:54:06 -0500

    enter page done
    welcome to capstone

---
+++ site/public/index.js
@@ -0,0 +1,39 @@
+// majority of the code here is from https://www.youtube.com/watch?v=y4K_5CVz7Cs
+
+const canvas = document.getElementById('enterCanvas');
+const context = canvas.getContext('2d')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const letters = "Python's Vanguard Capstone Team E Pandora's Box アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for( let x = 0; x < columns; x++ ) {
+	rainDrops[x] = 1;
+}
+
+const draw = () => {
+	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	context.fillStyle = '#0F0';
+	context.font = fontSize + 'px monospace';
+
+	for(let i = 0; i < rainDrops.length; i++)
+	{
+		const text = letters.charAt(Math.floor(Math.random() * letters.length));
+		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+		
+		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			rainDrops[i] = 0;
+        }
+		rainDrops[i]++;
+	}
+};
+
+setInterval(draw, 30);
