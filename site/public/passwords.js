d5dd43e4fd3b7edd7393948498629a0b074d7bf7
Luke Manary <69057730+xvyrq@users.noreply.github.com>
2022-11-14 10:56:23 -0600

    more password enhancements

--- site/public/passwords.js
+++ site/public/passwords.js
@@ -1,16 +1,24 @@
 function passCheck () {
-const password = document.getElementById("passInput").value;
-const box1 = document.getElementById("card1");
-const box2 = document.getElementById("card2"); 
-const box3 = document.getElementById("card3"); 
-const box4 = document.getElementById("card4");     
+    const password = document.getElementById("passInput").value;
+    const box1 = document.getElementById("card1");
+    const box2 = document.getElementById("card2"); 
+    const box3 = document.getElementById("card3"); 
+    const box4 = document.getElementById("card4");     
 
-if (password.length > 16) {
-    box1.style.borderColor = "green";    
-} else if (password.length = 0){
-    box1.style.borderColor = "white";
-}
-else {
-    box1.style.borderColor = "red";
-}
+    //length requirements
+    if (password.length >= 16) {
+        box1.style.borderColor = "green";    
+    } else if (password.length = 0){
+        box1.style.borderColor = "white";
+    }
+    else {
+        box1.style.borderColor = "red";
+    }
+
+    //no common phrases or names
+    var pw = require("pw");
+    var text = pw.readFileSync("100mostCommonPW.txt", "utf-8");
+        var textByLine = text.split("/n");
+    console.log(pw);
+    console.log(text);
 }   
